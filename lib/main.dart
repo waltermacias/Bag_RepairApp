73348736cc9371bfcad5c2413e3949190aea7ff8
waltermacias <61175490+waltermacias@users.noreply.github.com>
2021-12-02 00:52:54 -0500

    Basic project creation

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:bag_repair_prep/screens/login_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bag_Repair App',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
